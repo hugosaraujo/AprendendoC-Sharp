ee7c0484f7a4bd7955daccd1f5ecbc9e0750b551
hugoa <hugoa@DELL_HUGO>
2023-02-04 23:30:24 -0300

    Adicionar arquivos de projeto.

---
+++ 01-OlaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("Olá, mundo");
+        Console.WriteLine("Fim da execução do programa. Aperte o ENTER para encerrar a reprodução");
+        Console.ReadLine();
+    }
+}
